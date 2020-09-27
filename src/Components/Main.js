d0bba6c0644651077cfdbf51bfbd003a1515796b
Syed Abdullah Ali <mtuais95@gmail.com>
2020-09-27 15:06:32 +0400

    complete functionality of main component

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -24,6 +24,28 @@ export default class Main extends Component {
         this.setState({step:step-1})
     }
 
+    handleChange = input=> e=> {
+        e.preventDefault();
+        this.setState({[input]:e.target.value})
+    }
+
+    stepHandler = () => {
+        const {fname, lname, companyName, companyLocation, favDish, step} = this.state;
+        if (step == 1) {
+            return (
+                <First fname={fname} lname={lname} next={this.next} handleChange = {this.handleChange} />
+            )
+        } else if (step == 2) {
+            return (
+                <Second companyName={companyName} companyLocation={companyLocation} favDish={favDish} next={this.next} prev={this.prev} handleChange={this.handleChange} />
+            )
+        } else if (step == 3) {
+            return (
+                <Show fname={fname} lname={lname} companyName={companyName} companyLocation={companyLocation} favDish={favDish} prev={this.prev} />
+            )
+        }
+    }
+
     render() {
         return (
             <div>
